7651ae32d7f20d72637a22c666ce501101afa7d4
Samuel-Ngigi-web <samuelcscience@gmail.com>
2023-12-08 09:03:03 +0300

    add files to staging area

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const cors = require("cors");
+const greet = require('./routes/greet');
+
+const app = express();
+
+//middlewares
+app.use(express.json());
+app.use(cors());
+
+// adding routes
+app.use('/greet', greet);
+
+app.get('/', (req, res) => {
+	res.json('Welcome to the API');
+});
+
+
+
+
+//port
+const port = process.env.PORT || 5500;
+app.listen(port, () => console.log(`Listening on Port: ${port}`));

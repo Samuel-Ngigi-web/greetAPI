8d689dfd0aa5822d08f9adf11ab3947c63c8f89b
Samuel-Ngigi-web <108070575+Samuel-Ngigi-web@users.noreply.github.com>
2024-01-10 09:11:11 +0300

    Update callback metadata index.js

--- index.js
+++ index.js
@@ -41,15 +41,16 @@ app.post("/darajatest", function(req, res){
  //console.log(req.body.Body["stkCallback"])
    const newCallbackDetails = new DarajaCallback({
    // transacDetails: Body,
-   MerchantRequestID: req.body.Body.stkCallback.MerchantRequestID,
+    MerchantRequestID: req.body.Body.stkCallback.MerchantRequestID,
     CheckoutRequestID: req.body.Body.stkCallback.CheckoutRequestID,
     ResultCode: req.body.Body.stkCallback.ResultCode,
     ResultDesc: req.body.Body.stkCallback.ResultDesc,
     //callbackMetadata
-    TransactionAmount: req.body.Body.stkCallback.CallbackMetadata.item[0].Value,
-    MpesaCode: req.body.Body.stkCallback.CallbackMetadata.item[1].Value,
-    TransactionDate: req.body.Body.stkCallback.CallbackMetadata.item[3].Value,
-    PhoneNumber: req.body.Body.stkCallback.CallbackMetadata.item[4].Value,
+    //TransactionAmount: req.body.Body.stkCallback.CallbackMetadata.item[0].Value,
+     TransactionAmount: responseObject.Body.stkCallback.CallbackMetadata.Item.find(item => item.Name === "Amount").Value;
+   // MpesaCode: req.body.Body.stkCallback.CallbackMetadata.item[1].Value,
+    //TransactionDate: req.body.Body.stkCallback.CallbackMetadata.item[3].Value,
+   // PhoneNumber: req.body.Body.stkCallback.CallbackMetadata.item[4].Value,
   })
 
   newCallbackDetails.save();
